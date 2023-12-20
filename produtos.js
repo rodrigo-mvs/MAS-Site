78ca8d9d0ee1e39f26bcd1e4cf477245e633aadc
logic19 <44166339+mgLogicc@users.noreply.github.com>
2023-12-20 10:03:56 +0000

    cszvsdgvsd

--- produtos.js
+++ produtos.js
@@ -1,57 +1,3 @@
-/*
-var vm = function () {
-    console.log('ViewModel initiated...');
-    //---Variáveis locais
-    var self = this;
-    self.products = ko.observableArray([])
-
-
-    self.products = [
-        {
-            name: 'Python Online Course',
-            type: 'Online Course',
-            image: 'https://www.freecodecamp.org/news/content/images/2021/12/chris-ried-ieic5Tq8YMk-unsplash.jpg',
-            categorie: 'Programming',
-            price: 50,
-            description: 'Learn Python programming through this comprehensive online course.',
-            author: 'CodeTeach'
-            
-        },
-        {
-            name: 'JavaScript E-Book',
-            type: 'E-Book',
-            image: 'https://firmbee.com/wp-content/uploads/ebook-cover-javascript-from-beginner-to-advanced.png',
-            categorie: 'Programming',
-            price: 20,
-            description: 'Dive into JavaScript with this comprehensive e-book, suitable for all levels.',
-            author: 'Sarah Codington'
-        },
-        {
-            name: 'Piano Tutoring',
-            type: 'Tutoring',
-            image: 'https://musicteacher.com/wp-content/uploads/2021/02/in-person-music-lessons.jpg',
-            categorie: 'Music',
-            price: 60,
-            description: 'Receive personalized piano tutoring from experienced professionals.',
-            author: 'Miss Tiles'
-        },
-        {
-            name: 'Guittar Online Course',
-            type: 'Course',
-            image: 'https://s3.amazonaws.com/guitareo/gl-sales/og-image.jpg',
-            categorie: 'Course',
-            price: 50,
-            description: 'Master the guitar with this comprehensive online course covering various styles and techniques.',
-            author: 'John Doe'
-        },
-    
-    
-    ];
-}; */
-
-
-
-
 var vm = function () {
     console.log('ViewModel initiated...');
     var self = this;
@@ -174,3 +120,15 @@ $(document).ready(function () {
     console.log("ready!");
     ko.applyBindings(new vm());
 });
+
+
+let price = parseFloat(0)
+let numitems = parseInt(0)
+
+$('.cart').on('click', function(){ //quando clicam em qqlr botão c a class .cart isto tudo acontece
+    price = parseFloat(price) + parseFloat(10.79) //o 10,79 é o preco dos meus artigos todos ent fica mais facil, senao era preciso ires uscar o id de cada um dos artigos(onde tens o preço) e somares isso á variavel
+    numitems = parseFloat(numitems) + parseFloat(1)
+    $('#numItem').text(parseFloat(numitems)) // o #numitem é o id do sitio onde tou a meter o numero de artigos
+    $('#price').text(parseFloat(price)) // o #price é o id do sitio onde tou a meter o preço total
+    console.log("clicouuuuu")
+})
--- script.js
+++ script.js
@@ -29,3 +29,18 @@ $(document).ready(function () {
     console.log("ready!");
     ko.applyBindings(new vm());
 });
+
+
+
+let price = parseFloat(0)
+let numitems = parseInt(0)
+
+$('.cart').on('click', function(){ //quando clicam em qqlr botão c a class .cart isto tudo acontece
+    price = parseFloat(price) + parseFloat(10.79) //o 10,79 é o preco dos meus artigos todos ent fica mais facil, senao era preciso ires uscar o id de cada um dos artigos(onde tens o preço) e somares isso á variavel
+    numitems = parseFloat(numitems) + parseFloat(1)
+    $('#numItem').text(parseFloat(numitems)) // o #numitem é o id do sitio onde tou a meter o numero de artigos
+    $('#price').text(parseFloat(price)) // o #price é o id do sitio onde tou a meter o preço total
+    console.log("clicouuuuu")
+})
+
+
